1f42bf4d7ca313e648e9d40ce5e03bd9de4d6665
Gioele <gcageggi@gmail.com>
2020-10-31 16:02:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testrealsense/MainActivity.java
@@ -0,0 +1,277 @@
+package com.example.testrealsense;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.intel.realsense.librealsense.Colorizer;
+import com.intel.realsense.librealsense.Config;
+import com.intel.realsense.librealsense.DepthFrame;
+import com.intel.realsense.librealsense.DeviceList;
+import com.intel.realsense.librealsense.DeviceListener;
+import com.intel.realsense.librealsense.Extension;
+import com.intel.realsense.librealsense.Frame;
+import com.intel.realsense.librealsense.FrameSet;
+import com.intel.realsense.librealsense.GLRsSurfaceView;
+import com.intel.realsense.librealsense.Pipeline;
+import com.intel.realsense.librealsense.PipelineProfile;
+import com.intel.realsense.librealsense.RsContext;
+import com.intel.realsense.librealsense.StreamFormat;
+import com.intel.realsense.librealsense.StreamType;
+import com.intel.realsense.librealsense.VideoFrame;
+
+import static com.example.testrealsense.ImageUtils.*;
+
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "librs capture example";
+    private static final int PERMISSIONS_REQUEST = 0;
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+    private boolean mPermissionsGranted = false;
+
+    private Context mAppContext;
+    private TextView mBackGroundText;
+    private GLRsSurfaceView mGLSurfaceView;
+    private boolean mIsStreaming = false;
+    private final Handler mHandler = new Handler();
+
+    private Pipeline mPipeline;
+    private Colorizer mColorizer;
+    private RsContext mRsContext;
+
+    private Bitmap realsenseBM;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mAppContext = getApplicationContext();
+        mBackGroundText = findViewById(R.id.connectCameraText);
+        mGLSurfaceView = findViewById(R.id.glSurfaceView);
+        mGLSurfaceView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
+        if (!hasPermission()) {
+            requestPermission();
+            mPermissionsGranted = true;
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGLSurfaceView.close();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            final int requestCode, final String[] permissions, final int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(PERMISSION_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA) ||
+                    shouldShowRequestPermissionRationale(PERMISSION_STORAGE)) {
+                Toast.makeText(this,
+                        "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
+            }
+            requestPermissions(new String[]{PERMISSION_CAMERA, PERMISSION_STORAGE}, PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mPermissionsGranted)
+            init();
+        else
+            Log.e(TAG, "missing permissions");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mRsContext != null)
+            mRsContext.close();
+        stop();
+        mColorizer.close();
+        mPipeline.close();
+    }
+
+    private void init(){
+        //RsContext.init must be called once in the application lifetime before any interaction with physical RealSense devices.
+        //For multi activities applications use the application context instead of the activity context
+        RsContext.init(mAppContext);
+
+        //Register to notifications regarding RealSense devices attach/detach events via the DeviceListener.
+        mRsContext = new RsContext();
+        mRsContext.setDevicesChangedCallback(mListener);
+
+        mPipeline = new Pipeline();
+        mColorizer = new Colorizer();
+
+        try(DeviceList dl = mRsContext.queryDevices()){
+            if(dl.getDeviceCount() > 0) {
+                showConnectLabel(false);
+                start();
+            }
+        }
+    }
+
+    private void showConnectLabel(final boolean state){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mBackGroundText.setVisibility(state ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    private DeviceListener mListener = new DeviceListener() {
+        @Override
+        public void onDeviceAttach() {
+            showConnectLabel(false);
+        }
+
+        @Override
+        public void onDeviceDetach() {
+            showConnectLabel(true);
+            stop();
+        }
+    };
+
+    Runnable mStreaming = new Runnable() {
+        final DecimalFormat df = new DecimalFormat("#.##");
+
+        @Override
+        public void run() {
+            try {
+                try(FrameSet frames = mPipeline.waitForFrames()) {
+                    try (Frame f = frames.first(StreamType.COLOR)){
+                        VideoFrame color = f.as(Extension.VIDEO_FRAME);
+                        
+                        int c_size= color.getDataSize();
+                        int c_height = color.getHeight();
+                        int c_width = color.getWidth();
+                        byte[] c_data = new  byte[c_size];
+                        color.getData(c_data);
+                        final int len = c_data.length;
+                        if(c_data.length !=0) {
+
+                            realsenseBM = rgb2Bitmap(c_data,c_width,c_height);
+                            saveBitmap(realsenseBM,"realsense.png");
+                            //mySurfaceView.setBitmap(realsenseBM);
+
+                            Log.d(TAG, "onCaptureData: " + c_data.length);
+                            Log.d(TAG,"transform byte to bitmap successfully\n");
+
+                        }else{
+                            Log.d(TAG,"fail to load color data\n");
+                        }
+                    }
+                    try (Frame f = frames.first(StreamType.DEPTH))
+                    {
+                        DepthFrame depth = f.as(Extension.DEPTH_FRAME);
+                        final float deptValue = depth.getDistance(depth.getWidth()/2, depth.getHeight()/2);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView textView = findViewById(R.id.distanceTextView);
+                                textView.setText("Distance: " + df.format(deptValue));
+                            }
+                        });
+                    }
+                    try(FrameSet processed = frames.applyFilter(mColorizer)) {
+                        mGLSurfaceView.upload(processed);
+                    }
+                }
+                mHandler.post(mStreaming);
+            }
+            catch (Exception e) {
+                Log.e(TAG, "streaming, error: " + e.getMessage());
+            }
+        }
+    };
+
+    private void configAndStart() throws Exception {
+        try(Config config  = new Config())
+        {
+            config.enableStream(StreamType.DEPTH, 640, 480);
+            config.enableStream(StreamType.COLOR, 640, 480);
+            // try statement needed here to release resources allocated by the Pipeline:start() method
+            try(PipelineProfile pp = mPipeline.start(config)){}
+        }
+    }
+
+    private synchronized void start() {
+        if(mIsStreaming)
+            return;
+        try{
+            Log.d(TAG, "try start streaming");
+            mGLSurfaceView.clear();
+            configAndStart();
+            mIsStreaming = true;
+            mHandler.post(mStreaming);
+            Log.d(TAG, "streaming started successfully");
+        } catch (Exception e) {
+            Log.d(TAG, "failed to start streaming");
+        }
+    }
+
+    private synchronized void stop() {
+        if(!mIsStreaming)
+            return;
+        try {
+            Log.d(TAG, "try stop streaming");
+            mIsStreaming = false;
+            mHandler.removeCallbacks(mStreaming);
+            mPipeline.stop();
+            mGLSurfaceView.clear();
+            Log.d(TAG, "streaming stopped successfully");
+        } catch (Exception e) {
+            Log.d(TAG, "failed to stop streaming");
+        }
+    }
+}
